a782554bcf1c0a67bdf0f7c9603f90c071cee6bf
Mikail Halikov <mikail@halikov.com>
2019-06-08 02:09:46 +0300

    отображение поздравления работает, но при первой загрузке верхний отступ на 10 меньше чем после запуска игры без перезагрузки страницы

--- js/stat.js
+++ js/stat.js
@@ -29,15 +29,11 @@ var getMaxElement = function (arr) {
   return Math.max.apply(null, arr);
 };
 
-var renderGreetingText = function (text, x, y) {
-  ctx.fillText(text, x, y);
-};
-
-var getGreetingText = function (ctx, greetingText, x, y) {
-  renderGreetingText(firstStroke, CLOUD_X + cloudPadding, CLOUD_Y + cloudPadding, FONT, '#000');
-  renderGreetingText(secondStroke, CLOUD_X + cloudPadding, CLOUD_Y + cloudPadding + FONT_GAP, FONT, '#000');
-
-  ctx.fillText(greetingText, x, y);
+var renderGreetingText = function (ctx, positionX, positionY) {
+  ctx.fillStyle = '#000';
+  ctx.font = FONT;
+  ctx.fillText(firstStroke, positionX + cloudPadding, positionY + cloudPadding);
+  ctx.fillText(secondStroke, positionX + cloudPadding, positionY + cloudPadding + FONT_GAP);
 };
 
 var renderStatisticsBar = function (ctx, color, x, y, width, height) {
@@ -70,29 +66,7 @@ window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgba(255, 255, 255, 1)');
 
-  getGreetingText(greetingText, CLOUD_X, CLOUD_Y);
-
-  // var getPlayersHistagran = function (ctx, names, scores) {
-
-  //   var maxTime = getMaxElement(scores);
-  //   names.forEach(function (name, index) {
-  //     var barWidth = BAR_WIDTH;
-  //     var barHeight = (BAR_HEIGHT * scores[index]) / maxTime;
-  //     var barPositionY = BAR_Y - barHeight;
-  //     var barPositionX = BAR_X + (BAR_WIDTH + BAR_GAP) * index;
-
-  //     var myColor = 'hsl(0, 100%, 50%)';
-  //     var randomColor = 'hsl(240,' + Math.ceil(Math.random() * 100) + '%' + ', 50%)';
-  //     var barColor = name === 'Вы' ? myColor : randomColor;
-
-  //     renderStatisticsBar(ctx, barColor, barPositionX, barPositionY, barWidth, barHeight);
-
-  //     ctx.fillStyle = '#000';
-  //     ctx.textBaseline = 'hanging';
-  //     ctx.fillText(Math.round(scores[index]), barPositionX, barPositionY - FONT_GAP);
-  //     ctx.fillText(name, BAR_X + (BAR_GAP + BAR_WIDTH) * index, playersNameMarginTop + GAP);
-  //   });
-  // };
+  renderGreetingText(ctx, CLOUD_X, CLOUD_Y);
 
   getPlayersHistagran(ctx, players, times);
 };
