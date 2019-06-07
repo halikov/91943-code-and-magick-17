da0733d0a7339248402b4f7e7009ea7a4e567908
Mikail Halikov <mikail@halikov.com>
2019-06-08 00:54:06 +0300

    выводе всего в отдельные функции, ничего не работает.

--- js/stat.js
+++ js/stat.js
@@ -17,6 +17,9 @@ var BAR_Y = CLOUD_HEIGHT - (cloudPadding + GAP); // задает вехнюю т
 var playersNameMarginTop = CLOUD_HEIGHT - cloudPadding - GAP; // расположение по вертикали имени от земли
 var FONT = '16px PT Mono';
 
+var firstStroke = 'Ура вы победили!';
+var secondStroke = 'Список результатов:';
+
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
@@ -26,10 +29,15 @@ var getMaxElement = function (arr) {
   return Math.max.apply(null, arr);
 };
 
-var renderGreetingText = function (ctx, color, font, text, x, y) {
-  ctx.fillStyle = color;
-  ctx.font = font;
+var renderGreetingText = function (text, x, y) {
   ctx.fillText(text, x, y);
+};
+
+var getGreetingText = function (ctx, greetingText, x, y) {
+  renderGreetingText(firstStroke, CLOUD_X + cloudPadding, CLOUD_Y + cloudPadding, FONT, '#000');
+  renderGreetingText(secondStroke, CLOUD_X + cloudPadding, CLOUD_Y + cloudPadding + FONT_GAP, FONT, '#000');
+
+  ctx.fillText(greetingText, x, y);
 };
 
 var renderStatisticsBar = function (ctx, color, x, y, width, height) {
@@ -37,31 +45,54 @@ var renderStatisticsBar = function (ctx, color, x, y, width, height) {
   ctx.fillRect(x, y, width, height);
 };
 
-window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgba(255, 255, 255, 1)');
-  ctx.textBaseline = 'hanging';
-  renderGreetingText(ctx, '#000', FONT, 'Ура вы победили!', CLOUD_X + cloudPadding, CLOUD_Y + cloudPadding);
-  ctx.textBaseline = 'hanging';
-  renderGreetingText(ctx, '#000', FONT, 'Список результатов:', CLOUD_X + cloudPadding, CLOUD_Y + cloudPadding + FONT_GAP);
-
-  var maxTime = getMaxElement(times);
-
-  players.forEach(function (name, index) {
+var getPlayersHistagran = function (ctx, names, scores) {
+  var maxTime = getMaxElement(scores);
+  names.forEach(function (name, index) {
     var barWidth = BAR_WIDTH;
-    var barHeight = (BAR_HEIGHT * times[index]) / maxTime;
+    var barHeight = (BAR_HEIGHT * scores[index]) / maxTime;
     var barPositionY = BAR_Y - barHeight;
     var barPositionX = BAR_X + (BAR_WIDTH + BAR_GAP) * index;
 
-    if (name === ('Вы')) {
-      renderStatisticsBar(ctx, 'hsl(0, 100%, 50%)', barPositionX, barPositionY, barWidth, barHeight);
-    } else {
-      renderStatisticsBar(ctx, 'hsl(240,' + Math.ceil(Math.random() * 100) + '%' + ', 50%)', barPositionX, barPositionY, barWidth, barHeight);
-    }
+    var myColor = 'hsl(0, 100%, 50%)';
+    var randomColor = 'hsl(240,' + Math.ceil(Math.random() * 100) + '%' + ', 50%)';
+    var barColor = name === 'Вы' ? myColor : randomColor;
+
+    renderStatisticsBar(ctx, barColor, barPositionX, barPositionY, barWidth, barHeight);
 
     ctx.fillStyle = '#000';
     ctx.textBaseline = 'hanging';
-    ctx.fillText(Math.round(times[index]), barPositionX, barPositionY - FONT_GAP);
+    ctx.fillText(Math.round(scores[index]), barPositionX, barPositionY - FONT_GAP);
     ctx.fillText(name, BAR_X + (BAR_GAP + BAR_WIDTH) * index, playersNameMarginTop + GAP);
   });
+};
+
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'rgba(255, 255, 255, 1)');
+
+  getGreetingText(greetingText, CLOUD_X, CLOUD_Y);
+
+  // var getPlayersHistagran = function (ctx, names, scores) {
+
+  //   var maxTime = getMaxElement(scores);
+  //   names.forEach(function (name, index) {
+  //     var barWidth = BAR_WIDTH;
+  //     var barHeight = (BAR_HEIGHT * scores[index]) / maxTime;
+  //     var barPositionY = BAR_Y - barHeight;
+  //     var barPositionX = BAR_X + (BAR_WIDTH + BAR_GAP) * index;
+
+  //     var myColor = 'hsl(0, 100%, 50%)';
+  //     var randomColor = 'hsl(240,' + Math.ceil(Math.random() * 100) + '%' + ', 50%)';
+  //     var barColor = name === 'Вы' ? myColor : randomColor;
+
+  //     renderStatisticsBar(ctx, barColor, barPositionX, barPositionY, barWidth, barHeight);
+
+  //     ctx.fillStyle = '#000';
+  //     ctx.textBaseline = 'hanging';
+  //     ctx.fillText(Math.round(scores[index]), barPositionX, barPositionY - FONT_GAP);
+  //     ctx.fillText(name, BAR_X + (BAR_GAP + BAR_WIDTH) * index, playersNameMarginTop + GAP);
+  //   });
+  // };
+
+  getPlayersHistagran(ctx, players, times);
 };
