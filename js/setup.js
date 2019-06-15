3b91d0f43ab4426f82c2efea290d6cef17c99005
Mikail Halikov <mikail@halikov.com>
2019-06-15 12:55:22 +0300

    пункты 1 и 5 объединены в одну функцию. Исправлены имена переменых, выбор цветов глаз и плащей сделан случайным

--- js/setup.js
+++ js/setup.js
@@ -1,30 +1,30 @@
 'use strict';
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
 
-document.querySelector('.setup-similar').classList.remove('hidden');
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
+var showUserDialog = function () {
+  var userDialog = document.querySelector('.setup');
+  userDialog.classList.remove('hidden');
+  // можно открыть это. если нам потом понадобится обратиться к этому блоку, чтобы не писать путь к нему от его родителя.
+  // var setupSimilar = document.querySelector('.setup-similar');
+  // закрыть это. эта строчка экономит две (сверху и снизу) строчки. Если к setup-similar нам не нужно обращаться без setup.
+  userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  // setupSimilar.classList.remove('hidden');
+};
 
 var getRandom = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-var firstnames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var secondnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-
 var getUserNames = function () {
+  var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  var secondNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var userNames = [];
-  for (var i = 1; i < firstnames.length; i++) {
-    userNames.push(firstnames[getRandom(1, firstnames.length - 1)] + ' ' + secondnames[getRandom(1, secondnames.length - 1)]);
+  for (var i = 1; i < firstNames.length; i++) {
+    userNames.push(firstNames[getRandom(1, firstNames.length - 1)] + ' ' + secondNames[getRandom(1, secondNames.length - 1)]);
   }
 
   return userNames;
 };
-
 
 var getWizards = function () {
   var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -36,16 +36,18 @@ var getWizards = function () {
   for (var i = 0; i < 4; i++) {
     wizards.push({
       name: names[i],
-      coatColor: coatColors[i],
-      eyeColor: eyeColors[i]
+      coatColor: coatColors[getRandom(1, (coatColors.length - 1))],
+      eyeColor: eyeColors[getRandom(1, (eyeColors.length - 1))]
     });
   }
   return wizards;
 };
-
 var wizardsList = getWizards();
 
 var renderWizard = function (wizard) {
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
@@ -55,10 +57,15 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizardsList.length; i++) {
-  fragment.appendChild(renderWizard(wizardsList[i]));
-}
-similarListElement.appendChild(fragment);
+var renderAllWizards = function () {
+  var similarListElement = document.querySelector('.setup-similar-list');
+  var fragment = document.createDocumentFragment();
 
-userDialog.querySelector('.setup-similar').classList.remove('.hidden');
+  wizardsList.forEach(function (item) {
+    fragment.appendChild(renderWizard(item));
+  });
+  similarListElement.appendChild(fragment);
+};
+
+renderAllWizards();
+showUserDialog();
